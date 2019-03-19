bf19abfa0ba2aeb7f1eb3d581e6dd8399fca2bbe
pehdsa <pehdsa@gmail.com>
2019-03-19 18:01:53 -0400

    novo

---
+++ objeto/funcoesImportantes.js
@@ -0,0 +1,27 @@
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+// PEGA TODAS AS CHAVES DO OBJETO "pessoa"
+console.log(Object.keys(pessoa));
+
+// PEGA TODOS OS VALORES DO OBJETO "pessoa"
+console.log(Object.values(pessoa));
+
+// PEGA TODAS AS CHAVES E OS VALORES DO OBJETO "pessoa"
+console.log(Object.entries(pessoa));
+
+Object.entries(pessoa).forEach(e => {
+    console.log(`${e[0]} : ${e[1]}`)
+});
+
+Object.defineProperties(pessoa, 'dataNascimento', {
+    enumerable: true, // vai ser listada
+    writable: false, // não pode ser modificada
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017';
+console.log(pessoa.dataNascimento);
--- objeto/notacaoLiteral.js
+++ objeto/notacaoLiteral.js
@@ -5,3 +5,28 @@ const c = 3;
 const obj1 = { a:a, b:b, c:c }
 const obj2 = { a, b, c } // mesma coisa que o de cima pois o atributo e a variavel tem o mesmo nome
 console.log(obj1,obj2);
+
+//
+
+const nomeAttr  = 'nota';
+const valorAttr = 7.8;
+
+const obj3 = {};
+obj3[nomeAttr] = valorAttr;
+console.log(obj3);
+
+const obj4 = { [nomeAttr] : valorAttr }
+console.log(obj4);
+
+const obj5 = {
+    //ANTIGA
+    funcao1 : function(){
+        // ...
+    },
+    //NOVA
+    funcao2(){
+        // ...
+    }
+
+}
+console.log(obj5);
