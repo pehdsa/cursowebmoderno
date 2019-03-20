291f3d62b4497f9ad05e6f1d719a095521c65a2f
pehdsa <pehdsa@gmail.com>
2019-03-20 17:55:04 -0400

    novo

--- objeto/funcoesImportantes.js
+++ objeto/funcoesImportantes.js
@@ -17,11 +17,11 @@ Object.entries(pessoa).forEach(e => {
     console.log(`${e[0]} : ${e[1]}`)
 });
 
-Object.defineProperties(pessoa, 'dataNascimento', {
+Object.defineProperty(pessoa, 'dataNascimento', {
     enumerable: true, // vai ser listada
     writable: false, // não pode ser modificada
     value: '01/01/2019'
-})
+});
 
-pessoa.dataNascimento = '01/01/2017';
+pessoa.dataNascimento = '01/01/2017'; // Não vai alterar pois travou o objeto com o "writable : false"
 console.log(pessoa.dataNascimento);
