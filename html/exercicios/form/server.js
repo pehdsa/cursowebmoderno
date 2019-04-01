358271b5c8807723a4a0521dd45bbb3d66183066
pehdsa <pehdsa@gmail.com>
2019-04-01 17:40:47 -0400

    novo

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body)
+    res.send('Parabéns')
+})
+
+app.listen(3003);
