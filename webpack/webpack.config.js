1f7ad5c0d3119e0c2a29bb529c0faec5d85f896c
pehdsa <pehdsa@gmail.com>
2019-04-10 17:46:38 -0400

    novas

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer : {
+        contentBase: './public',
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -23,6 +39,9 @@ module.exports = {
                     'css-loader', // interpreta os @imports e urls
                     'sass-loader'
                 ]
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
             }
         ]
     }
